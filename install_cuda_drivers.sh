78f6df2af410984e226daa856720e1d649446c15
Sourya Kakarla <sourya4@ladduu.com>
2023-02-01 12:16:04 -0500

    Remove patch in cuda drivers script
    Removing patch installation as it's not present in the original instructions in courseworks.
    TODO: check if patch is required and need to be added.

--- install_cuda_drivers.sh
+++ install_cuda_drivers.sh
@@ -1,7 +1,3 @@
 wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/patches/1/cuda_10.2.1_linux.run
-sudo sh
 echo "Running driver installation script"
-sh ./cuda_10.2.89_440.33.01_linux.run
-echo "Running patch installation script"
-sh ./cuda_10.2.1_linux.run
+sudo sh ./cuda_10.2.89_440.33.01_linux.run
